30225faa4fa26c5b4ec250e99db849f6a3709e3b
Irina Kramer <irakramer@gmail.com>
2020-01-13 23:07:34 -0800

    Updates

---
+++ precache-manifest.a2451d54f2e43cad9dc9d313b94328de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e305ac898464fc715bafb1d3154ead37",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b78a5d1896a6709a9b58",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9d162c0ff2fe16b4f76e",
+    "url": "/tic-tac-toe/static/js/2.26b8bc9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.26b8bc9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b78a5d1896a6709a9b58",
+    "url": "/tic-tac-toe/static/js/main.322c6961.chunk.js"
+  },
+  {
+    "revision": "37b1a36cd7995ee344a0",
+    "url": "/tic-tac-toe/static/js/runtime-main.7549cc49.js"
+  }
+]);
